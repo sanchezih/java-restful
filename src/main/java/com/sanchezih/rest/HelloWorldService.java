c9da9924fed235d7c3fd1adf46949bfed86febdb
Name <Email>
2017-05-02 14:41:00 -0300

    ++ comments

--- src/main/java/com/sanchezih/rest/HelloWorldService.java
+++ src/main/java/com/sanchezih/rest/HelloWorldService.java
@@ -6,13 +6,14 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
 
 @Path("/hello")
+// http://localhost:8080/java-restful/api/hello/ignacio
 public class HelloWorldService {
 
 	@GET
 	@Path("/{param}")
 	public Response getMsg(@PathParam("param") String msg) {
 
-		String output = "Jersey says : " + msg;
+		String output = "Jersey dice: " + msg;
 
 		return Response.status(200).entity(output).build();
 
